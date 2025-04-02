61c640bfcb20dfc4bec087e38012e2ecd842ca25
dev-alexandre2024 <alexandrecdsdev@gmail.com>
2025-04-02 11:00:44 -0300

    adicionando os conhecimentos de javascript em um formulario

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function validarformulario(event){
     var nome = document.getElemenById('nome').value;
-    var idade = parseInt(getElemenById('idade').value);
+    var idade = parseInt(document.getElemenById('idade').value);
     var cidade = document.getElemenById('cidade').value;
 
-     if( !nome||!idade||!cidade ){
-        document.window.alert("por favor,preencha todos os campos");
-        return
-     }
 
     let saberidade = "";
     if (idade < 18){
-        saberidade =`você tem apenas ${idade} anos voce e menor de idade`
+        resultado +=`você tem apenas ${idade} anos voce e menor de idade`;
     }
     else if(idade > 18){
-        saberidade =`você tem ${idade} você é maior de idade`
+         resultado +=`você tem ${idade} você é maior de idade`;
     }
+
+
+     resultado += `<br>Nome ${nome}`;
+     resultado += `<br>Cidade ${cidade}`;
+
+     document.getElementById('resultado').innerHTML.resultado;
 }
